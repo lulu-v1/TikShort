9f766f97b3a031cd69216ce74c5d64b891b34e55
lulu-v2 <luca.izidi@gmail.com>
2024-04-09 12:46:18 +0200

    Add: First carousels

---
+++ src/components/AdvancedCarousel.js
@@ -0,0 +1,56 @@
+import { useSnapCarousel } from 'react-snap-carousel';
+
+const AdvancedCarousel = () => {
+    const { scrollRef, pages, activePageIndex, next, prev, goTo } =
+        useSnapCarousel();
+    return (
+        <>
+            <ul
+                ref={scrollRef}
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    overflow: 'auto',
+                    scrollSnapType: 'y mandatory'
+                }}
+            >
+                {Array.from({ length: 100 }).map((_, i) => (
+                    <li
+                        style={{
+                            backgroundColor: 'aqua',
+                            fontSize: '50px',
+                            width: '250px',
+                            height: '250px',
+                            flexShrink: 0,
+                            color: '#fff',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}
+                    >
+                        Item {i}
+                    </li>
+                ))}
+            </ul>
+            <div>
+                {activePageIndex + 1} / {pages.length}
+            </div>
+            <button onClick={() => prev()}>Prev</button>
+            <button onClick={() => next()}>Next</button>
+            <ul style={{ display: 'flex',flexDirection:'column'}}>
+                {pages.map((_, i) => (
+                    <li key={i}  >
+                        <button
+                            style={i === activePageIndex ? { opacity: 0.5 } : {}}
+                            onClick={() => goTo(i)}
+                        >
+                            {i + 1}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default AdvancedCarousel;

e33a67019b5bc8169cdae66b68d6859f5aa0b0cc
lulu-v2 <luca.izidi@gmail.com>
2024-04-16 13:56:47 +0200

    Add: timed text displaying FINISHED

--- src/components/VideoCarousel.js
+++ src/components/VideoCarousel.js
@@ -4,7 +4,9 @@ import ScrollSnapping from "../scripts/ScrollSnapping";
 const VideoCarousel = () => {
     const [videos, setVideos] = useState([]); // State to store video elements
     const [focusedIndex, setFocusedIndex] = useState(0); // State to store the index of the focused element
-    const [currentTime, setCurrentTime] = useState(0); // State to store the current time of the video
+    const [startIndex, setStartIndex] = useState(0); // State to store the amount of words to display
+    const [currentText, setCurrentText] = useState(''); // State to store the current text of the video
+
     // Define video URLs
     const videoURLs = [
         '/static/vids/28707-371213524_tiny.mp4',
@@ -12,18 +14,44 @@ const VideoCarousel = () => {
         '/static/vids/199294-909903183_small.mp4',
         '/static/vids/202560-918431383_tiny.mp4',
     ];
-    const handleTimeUpdate = (event) => {
+
+    function splitSentence(sentence, chunkSize) {
+        const words = sentence.split(' ');
+        const chunks = [];
+        for (let i = 0; i < words.length; i += chunkSize) {
+            chunks.push(words.slice(i, i + chunkSize).join(' '));
+        }
+        return chunks.map(chunk => chunk + '     ');
+    }
+
+    const handleTextUpdate = (event) => {
+        const chunkSize = 5;
+        const fullText = 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Wesh aussi on rajoute ca';
         const video = event.target;
-        setCurrentTime(video.currentTime);
+        console.log(splitSentence(fullText, chunkSize))
+        const chunks =  splitSentence(fullText, chunkSize);
+
+
+
+        const durationPerTrunk = video.duration / chunks.length;
+        const durationPerWord = durationPerTrunk / (chunkSize+5);
+
+        const trunkToDisplay = Math.floor(video.currentTime / durationPerTrunk);
+        const endIndex = Math.floor((video.currentTime % durationPerTrunk) / durationPerWord);
+        const wordsToDisplay = chunks[trunkToDisplay].split(' ').slice(0, endIndex + 1);
+        setCurrentText(wordsToDisplay.join(' '));
     };
 
-    function PlayFocusedVideo() {
-        const videos = document.querySelectorAll('video');
 
+    function PlayFocusedVideo() {
+        setStartIndex(0);
+        const videos = document.querySelectorAll('video');
         videos.forEach((video, index) => {
-            if (video.key !== focusedIndex)
-            {
-                video.pause();
+            if (video.readyState >= HTMLMediaElement.HAVE_METADATA) {
+                video.currentTime = 0;
+                if (index !== focusedIndex && !video.paused) {
+                    video.pause();
+                }
             }
         });
 
@@ -35,19 +63,17 @@ const VideoCarousel = () => {
         }
     }
 
-
     useEffect(() => {
         const videoElements = videoURLs.map((url, i) => {
             return (
                 <video
                     key={i}
                     src={url}
-                    // autoPlay={i === focusedIndex}
                     loop
                     style={{
                         height: '100%',
                     }}
-                    onTimeUpdate={handleTimeUpdate}
+                    onTimeUpdate={handleTextUpdate}
                 />
             );
         });
@@ -55,13 +81,15 @@ const VideoCarousel = () => {
 
         window.addEventListener('scroll', PlayFocusedVideo);
 
-
+        return () => {
+            window.removeEventListener('scroll', PlayFocusedVideo);
+        }
     }, [focusedIndex]);
 
     const handleFocusedIndexChange = (index) => {
         setFocusedIndex(index);
-        setCurrentTime(0);
     };
+
     return (
         <div style={{overflow: 'hidden'}}>
             <ul
@@ -75,7 +103,6 @@ const VideoCarousel = () => {
                     <li
                         key={i}
                         style={{
-                            outline: 'solid black 3px',
                             borderRadius: '10px',
                             width: '420px',
                             height: '750px',
@@ -90,8 +117,8 @@ const VideoCarousel = () => {
                     >
                         {video}
                         <p
-                        style={{fontSize: '3rem', position: 'absolute' }}
-                        >{currentTime}</p>
+                            style={{fontSize: '2.5rem',fontWeight:'bold', position: 'absolute', width: '350px'}}
+                        >{currentText}</p>
                     </li>
                 ))}
             </ul>

f6c0f21a5f4c8298c7c4631e18ec9a9a01d7e9a8
lulu-v2 <luca.izidi@gmail.com>
2024-05-24 00:42:42 +0200

    Add : DbManagement, Cors, Express

--- src/scripts/Init_db.js
+++ src/scripts/Init_db.js
@@ -5,14 +5,12 @@ const initializeDatabase = () => {
         db.run(`CREATE TABLE IF NOT EXISTS users (
             user_id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL UNIQUE,
-            email TEXT NOT NULL UNIQUE
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
         );`);
         db.run(`CREATE TABLE IF NOT EXISTS videos (
             video_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER,
-            title TEXT NOT NULL,
-            description TEXT,
-            FOREIGN KEY (user_id) REFERENCES users (user_id)
+            path TEXT NOT NULL
         );`);
         db.run(`CREATE TABLE IF NOT EXISTS comments (
             comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
